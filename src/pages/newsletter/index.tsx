13e82e5999ff7fe2a0d49bea7868a73c5bcf3e0c
nickfrosty <nfrostbutter@gmail.com>
2023-02-20 14:13:56 -0500

    feat: added newsletter page

---
+++ src/pages/newsletter/index.tsx
@@ -0,0 +1,94 @@
+import { NextSeoProps } from "next-seo";
+import DefaultLayout from "@/layouts/default";
+import PageHero from "@/components/core/PageHero";
+
+import styles from "@/styles/core/sidebar.module.css";
+import ContentCard from "@/components/core/ContentCard";
+
+// define the on-page seo metadata
+const seo: NextSeoProps = {
+  title: "Newsletter",
+  description: "",
+};
+
+export default function Page() {
+  return (
+    <DefaultLayout seo={seo}>
+      <PageHero className="container">
+        <h1 className="text-6xl tracking-normal">Newsletter</h1>
+
+        <p className="max-w-lg mx-auto text-xl">
+          The latest news and updates from the Solana Foundation.
+        </p>
+      </PageHero>
+
+      <main className={styles.wrapper + " container"}>
+        <ContentCard
+          // isExternal={true}
+          href="/newsletter/slug"
+          title="Solana Tech Roundup"
+          authorLabel="Jacob Creech"
+          // authorHref="#"
+          imageSrc="/img/content/newsletter_cover.jpg"
+          description="Solana Tech Roundup This past week there has been progress on offline message signing. This highly requested feature."
+        />
+        <ContentCard
+          href="/newsletter/slug"
+          title="Solana Tech Roundup"
+          authorLabel="Solana Foundation"
+          // authorHref="#"
+          imageSrc="/img/content/newsletter_cover.jpg"
+          description="Solana Tech Roundup This past week there has been progress on offline message signing. This highly requested feature."
+        />
+        <ContentCard
+          href="/newsletter/slug"
+          title="Solana 101"
+          authorLabel="Solana Foundation"
+          // authorHref="#"
+          imageSrc="/img/content/newsletter_cover.jpg"
+          description="Getting started with Solana by doing all the basic essentials from setup to deploying a program, to interacting with that program."
+        />
+        <ContentCard
+          href="/newsletter/slug"
+          title="Intro to Programming on Solana"
+          authorLabel="Solana Foundation"
+          // authorHref="#"
+          imageSrc="/img/content/newsletter_cover.jpg"
+          description="Introductory guide for building a smart contract on Solana using a simple escrow program example."
+        />
+        <ContentCard
+          href="/newsletter/slug"
+          title="Solana Tech Roundup"
+          authorLabel="Nader Dabit"
+          // authorHref="#"
+          imageSrc="/img/content/newsletter_cover.jpg"
+          description="Solana Tech Roundup This past week there has been progress on offline message signing. This highly requested feature."
+        />
+        <ContentCard
+          href="/newsletter/slug"
+          title="Solana Tech Roundup"
+          authorLabel="Solana Foundation"
+          // authorHref="#"
+          imageSrc="/img/content/newsletter_cover.jpg"
+          description="Solana Tech Roundup This past week there has been progress on offline message signing. This highly requested feature."
+        />
+        <ContentCard
+          href="/newsletter/slug"
+          title="Solana 101"
+          authorLabel="Solana Foundation"
+          // authorHref="#"
+          imageSrc="/img/content/newsletter_cover.jpg"
+          description="Getting started with Solana by doing all the basic essentials from setup to deploying a program, to interacting with that program."
+        />
+        <ContentCard
+          href="/newsletter/slug"
+          title="Intro to Programming on Solana"
+          authorLabel="Solana Foundation"
+          // authorHref="#"
+          imageSrc="/img/content/newsletter_cover.jpg"
+          description="Introductory guide for building a smart contract on Solana using a simple escrow program example."
+        />
+      </main>
+    </DefaultLayout>
+  );
+}
