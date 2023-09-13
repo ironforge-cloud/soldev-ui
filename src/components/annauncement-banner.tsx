217b8babf7af7197721d0b4a1661dec8235582f0
Italo Casas <me@italo.dev>
2023-09-13 15:39:34 -0400

    small change

--- src/components/annauncement-banner.tsx
+++ src/components/annauncement-banner.tsx
@@ -29,7 +29,7 @@ export default function AnnauncementBanner() {
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold">Announcing Ironforge</strong>
+          <strong className="font-semibold">Ironforge</strong>
           <svg
             viewBox="0 0 2 2"
             className="mx-2 inline h-0.5 w-0.5 fill-current"
@@ -37,14 +37,13 @@ export default function AnnauncementBanner() {
           >
             <circle cx={1} cy={1} r={1} />
           </svg>
-          Your Gateway to Serverless Solana Development
+          Accelerate your Solana development with Ironforge
         </p>
         <Link
-          href="https://www.ironforge.cloud/blog/ironforge-your-gateway-to-serverless-solana-development"
-          target="_blank"
+          href="https://www.ironforge.cloud"
           className="mx-auto flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
         >
-          Read <span aria-hidden="true">&rarr;</span>
+          Go <span aria-hidden="true">&rarr;</span>
         </Link>
       </div>
     </div>
