f96737c4dc2c4684210282f241f64a36b8d6e895
Mike MacCana <mike.maccana@gmail.com>
2024-05-03 13:33:28 -0400

    Remove reference to 'draft' for solana-course it's not used anymore

--- src/utils/fetch-course.ts
+++ src/utils/fetch-course.ts
@@ -1,7 +1,7 @@
 import { CourseStructure, Translations } from '../lib/types';
 import { get } from 'fetch-unfucked';
 
-const BRANCH: 'main' | 'draft' = 'main';
+const BRANCH: string = 'main';
 
 export const fetchLessonText = async (slug: string, locale?: string): Promise<string> => {
   const url = `https://raw.githubusercontent.com/Unboxed-Software/solana-course/${BRANCH}/content${
